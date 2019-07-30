6e073b506a89db7a0bd5b8f5b4e20f4f08f27882
Jokky <20024jok@163.com>
2019-07-30 17:22:21 +0800

    修改类名

--- src/main/java/com/javaproject/jcode/App.java
+++ src/main/java/com/javaproject/jcode/App.java
@@ -1,7 +1,7 @@
 package com.javaproject.jcode;
 
 import com.javaproject.jcode.collections.ArrayList_runoob;
-import com.javaproject.jcode.collections.FailFastMechanism;
+import com.javaproject.jcode.collections.FailFastMechanism_skywang;
 import com.javaproject.jcode.collections.LinkedList_skywang;
 import com.javaproject.jcode.collections.Vector_skywang;
 
@@ -15,7 +15,7 @@ public class App
     public static void main(String[] args )
     {
     	ArrayList_runoob.main(args);
-    	FailFastMechanism.main(args);
+    	FailFastMechanism_skywang.main(args);
     	LinkedList_skywang.main(args);
     	Vector_skywang.main(args);;
     	
